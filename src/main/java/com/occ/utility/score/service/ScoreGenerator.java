6ec653401e7e290192dc5defad7269b28c543f2e
hemanthv17 <46658333+hemanthv17@users.noreply.github.com>
2021-11-08 18:05:52 -0600

    removed comments

--- src/main/java/com/occ/utility/score/service/ScoreGenerator.java
+++ src/main/java/com/occ/utility/score/service/ScoreGenerator.java
@@ -1,14 +1,7 @@
-/**
- * 
- */
 package com.occ.utility.score.service;
 
 import java.io.File;
 
-/**
- * @author hemanthvuyyuru
- *
- */
 public interface ScoreGenerator {
 	public long calculateScore(File file);
 }
